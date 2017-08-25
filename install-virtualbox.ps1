31f26f3a5f670d6207a736997973afc639f2e2c5
koginoadm <31156607+koginoadm@users.noreply.github.com>
2017-08-26 02:12:12 +0900

    Update install-virtualbox.ps1

--- install-virtualbox.ps1
+++ install-virtualbox.ps1
@@ -1,7 +1,7 @@
 #@setlocal enableextensions enabledelayedexpansion & set "PATH0=%~f0" & PowerShell.exe -Command "& (Invoke-Expression -Command ('{#' + ((Get-Content '!PATH0:'=''!') -join \"`n\") + '}'))" %* & exit /b !errorlevel!
 # 
 # - Name
-#     install-vim.bat
+#     install-virtualbox.ps1
 # 
 # - Contents
 #     Install Vim.
