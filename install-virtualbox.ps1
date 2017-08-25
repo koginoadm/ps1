d271fa324f2c1f6f11c5d567ccbdec0dbf20827d
koginoadm <31156607+koginoadm@users.noreply.github.com>
2017-08-26 00:48:36 +0900

    Update install-virtualbox.ps1

--- install-virtualbox.ps1
+++ install-virtualbox.ps1
@@ -1,11 +1,41 @@
-# vars
+@setlocal enableextensions enabledelayedexpansion & set "PATH0=%~f0" & PowerShell.exe -Command "& (Invoke-Expression -Command ('{#' + ((Get-Content '!PATH0:'=''!') -join \"`n\") + '}'))" %* & exit /b !errorlevel!
+# 
+# - Name
+#     install-vim.bat
+# 
+# - Contents
+#     Install Vim.
+#
+# - Install
+#     powershell.exe -Command "Invoke-RestMethod -Uri "https://djeeno.github.io/ps1/install-vim.bat" -OutFile "$env:USERPROFILE\install-vim.bat""
+#
+# - Reference
+#     Vim - KaoriYa - https://www.kaoriya.net/software/vim/
+# 
+# - Revision
+#     2016-05-24 created.
+#     2016-06-08 modified.
+#     yyyy-MM-dd modified.
+# 
+
+
+################
+# constant
+################
+$vBase = 'http://download.virtualbox.org/virtualbox/'
+
+################
+# variables
+################
 $tmpd = "$env:USERPROFILE\.tmp.vbx"; if (-Not(Test-Path -Path $tmpd)) { mkdir $tmpd }
 $tmpf = "$tmpd\.tmp.virtualbox.log"
-$vBase = 'http://download.virtualbox.org/virtualbox/'
 $vStableUri = "$vBase$(Invoke-RestMethod -Uri $vBase | %{$_ -creplace '.*href="([^"]*)".*','$1'} > $tmpf ; cat $tmpf | ?{ $_ -match "^[0-9]+`.[0-9]+`.[0-9]+/" } | select -Last 1; rm $tmpf)"
 $vInstUri = "$vStableUri$(Invoke-RestMethod -Uri $vStableUri | %{$_ -creplace '.*href="([^"]*)".*','$1'} > $tmpf ; cat $tmpf | ?{ $_ -match "`.exe$" } | select -Last 1; rm $tmpf)"
 $vInstExe = "$env:USERPROFILE\$(Split-Path -Leaf $vInstUri)"
 
+################
+# main
+################
 # Administrator
 if (-Not([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator'))
 {
