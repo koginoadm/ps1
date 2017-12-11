da61be645e17e4dde1328f7e58d7b2a2bb523b31
koginoadm <31156607+koginoadm@users.noreply.github.com>
2017-12-11 16:54:33 +0900

    Update edit-hosts.bat

--- edit-hosts.bat
+++ edit-hosts.bat
@@ -7,7 +7,7 @@ rem # - Contents
 rem #     C:\Windows\System32\runas.exe notepad C:\Windows\System32\drivers\etc\hosts
 rem # 
 rem # - Install
-rem #     powershell.exe -Command "Invoke-RestMethod -Uri 'https://koginoadm.github.io/ps1/edit-hosts.bat' -OutFile "$env:USERPROFILE\edit-hosts.bat""
+rem #     powershell.exe -Command "Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/koginoadm/ps1/master/edit-hosts.bat' -OutFile "$env:USERPROFILE\edit-hosts.bat""
 rem # 
 rem # - Revision
 rem #     2017-05-10 created.
